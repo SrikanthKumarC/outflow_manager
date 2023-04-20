6a0c5f905a2e84825f1721602ef71969b1b074ef
Srikanth Kumar <wpsrikanth@gmail.com>
2023-04-19 20:56:38 -0700

    experimental

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import "./Login.css";
 const Login = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, loginWithPopup } = useAuth0();
   const { logout } = useAuth0();
 
   return (
@@ -11,7 +11,15 @@ const Login = () => {
         <h1>Expense Outflow Manager</h1>
         <p>Login to continue</p>
         <div className="btn_container">
-          <button onClick={() => loginWithRedirect()}>Log In</button>
+          <button
+            onClick={loginWithRedirect({
+              authorizationParams: {
+                redirect_uri: window.location.origin + '/home',
+              },
+            })}
+          >
+            Log In
+          </button>
           <button
             onClick={() =>
               logout({ logoutParams: { returnTo: window.location.origin } })
