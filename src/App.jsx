2119d7ce71b8afb25a7f0d1cf356dbbb6a8712de
Srikanth <66662854+SrikanthKumarC@users.noreply.github.com>
2023-06-30 11:25:21 +0530

    api bug

--- src/App.jsx
+++ src/App.jsx
@@ -99,7 +99,7 @@ function App() {
   useEffect(() => {
     if (limit === 0 || !limit) return;
     if (expenseG > limit) {
-      axios.post(`https://apparent-nepal-date-significantly.trycloudflare.com/${limit}`);
+      axios.post(`https://localhost:3000/${limit}`);
       setLocalLimit(0);
       dispatch(update(localLimit))
     }
